5ae910316b5f185cfd8b9687a39d16946d1acb0b
iipratte <iipratte@byu.edu>
2026-02-25 15:03:05 -0700

    Revert "updated shared layout"

---
+++ Mission8_3-15/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Mission8_3_15.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
