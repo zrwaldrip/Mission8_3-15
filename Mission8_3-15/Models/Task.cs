70b58002dc5e890ce37663548c1745d8df7529b8
zrwaldrip <zrwaldrip@gmail.com>
2026-02-25 14:17:29 -0700

    Set up the database

---
+++ Mission8_3-15/Models/Task.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Mission8_3_15.Models;
+
+public class Task
+{
+    [Key]
+    [Required]
+    public int TaskId { get; set; }
+    [Required]
+    public string TaskName { get; set; }
+    
+    public string? DueDate { get; set; }
+    
+    [Required]
+    public int Quadrant { get; set; }
+    
+    public string? Category { get; set; }
+    
+    [Required]
+    public bool Completed { get; set; }
+    
+    
+}
