86c201ce846fb800f2990cd5056f740bcfd7dbe1
zrwaldrip <zrwaldrip@gmail.com>
2026-02-25 13:50:45 -0700

    Initial commit

---
+++ Mission8_3-15/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Mission8_3_15.Models;
+
+namespace Mission8_3_15.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
