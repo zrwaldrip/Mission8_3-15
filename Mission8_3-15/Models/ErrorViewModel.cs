67c3ff5b713677114b5a8c6cefb3d16bd92172a2
iipratte <iipratte@byu.edu>
2026-02-25 14:55:06 -0700

    updated shared layout

--- Mission8_3-15/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Mission8_3_15.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
